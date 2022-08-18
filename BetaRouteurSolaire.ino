90a8e65a9ab639eb02104bc306a5527df42df56d
Julien AMREIN <github.julien@gmail.com>
2022-08-18 21:46:19 +0200

    mise en page

--- BetaRouteurSolaire.ino
+++ BetaRouteurSolaire.ino
@@ -8,7 +8,7 @@ int ledPin = 9;
 void setup()
 {  
   Serial.begin(9600);
-  
+
   emon1.voltage(2, 300, 1.7);  // Voltage: input pin, calibration, phase_shift
   emon1.current(1, 57);       // Current: input pin, calibration.
 
@@ -17,57 +17,48 @@ void setup()
 void loop()
 {
   emon1.calcVI(20,500);         // Calculate all. No.of half wavelengths (crossings), time-out
- // emon1.serialprint();           // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
-  
+
   float realPower       = emon1.realPower;        //extract Real Power into variable
   float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
   float powerFActor     = emon1.powerFactor;      //extract Power Factor into Variable
   float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
   float Irms            = emon1.Irms;             //extract Irms into Variable
-  
-  
- // double Irms = emon1.calcIrms(1480);
-  
+
   Serial.print(realPower);	       // Puissance apparente
-  Serial.print(" watt          ");
+  Serial.print(" signe          ");
   Serial.print(Irms);		       // Irms
   Serial.print(" Amperes        ");
-  Serial.print(valeurLedDimmer);		       // Irms
+  Serial.print(valeurLedDimmer);
   Serial.println(" valeur de la led");
   
-  
-
-  
  
-    if (realPower > 0) // on diminue la puissance autorisée dans le dimmer
+//------------------------------------------------------------------------------------------------------------
+  if (realPower > 0) // Si on consomme du courant depuis EDF, on diminue la puissance autorisée dans le dimmer
   {
-      if(valeurLedDimmer > 0)
-      {
-                    valeurLedDimmer -= 1;
-        analogWrite(ledPin, valeurLedDimmer);
+    if(valeurLedDimmer > 0)
+    {
+      valeurLedDimmer -= 1;
+      analogWrite(ledPin, valeurLedDimmer);
 
-      }
- 
+    }
+
   }
-  
-  
-    if (realPower < 0) // on augmente la puissance autorisée dans le dimmer
+
+
+  if (realPower < 0) // Si on injecte du courant vers EDF,on augmente la puissance autorisée dans le dimmer
   {
-      if(valeurLedDimmer <= 25)
-      {
-            valeurLedDimmer += 1;
-                    analogWrite(ledPin, valeurLedDimmer);
+    if(valeurLedDimmer <= 25)
+    {
+      valeurLedDimmer += 1;
+      analogWrite(ledPin, valeurLedDimmer);
 
-      }
-  
+    }
+
   }
-  
-  
-  
-  
-  
-  
-  
-  
+
+//------------------------------------------------------------------------------------------------------------
+
+
 
 }
+
